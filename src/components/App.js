711da1408824fc4a373b32f6f2a51ae4133bfc46
Satish Rathod <satishrathod.dev@gmail.com>
2024-07-29 09:03:21 +0530

    finished

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,51 @@
-import React from 'react'
+import React, { useState } from "react";
 
 const App = () => {
+  const [input, setInput] = useState("");
+  const [items, setItems] = useState([]);
+
+  const handleInput = (event) => {
+    setInput(event.target.value);
+  };
+
+  const addItem = () => {
+    // setItems((t) => [...t, "list"]);
+    if (input.trim() !== "") {
+      setItems((prevItems) => [...prevItems, input]);
+      setInput("");
+    }
+  };
+
   return (
     <div id="main">
-               {/* Do not remove the main div */}
+      {/* Do not remove the main div */}
+      <input
+        id="input-value"
+        type="text"
+        value={input}
+        onChange={handleInput}
+      />
+      <button onClick={addItem}>Add Item</button>
+      {/* <ol id="relativeList">
+        {items.map((item, index) => (
+          <li id={`relativeListItem${index + 1}`} key={index}>
+            {item}
+          </li>
+        ))}
+      </ol> */}
+      <ol id="relativeList">
+        {items.map((item, index) => {
+          const itemId = `relativeListItem${index + 1}`;
+          console.log(itemId); // Log the id
+          return (
+            <li id={itemId} key={index}>
+              {item}
+            </li>
+          );
+        })}
+      </ol>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
